6b2cf43625655a4cacb680b920c9bc6d693bbd66
Jarzt <dev.luanpham@gmail.com>
2024-05-02 11:24:48 +0700

    feat: find all factors

--- src/features/factor/factor.controller.ts
+++ src/features/factor/factor.controller.ts
@@ -1,34 +1,25 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { FactorService } from './services/factor.service';
-import { CreateFactorDto } from './dto/create-factor.dto';
-import { UpdateFactorDto } from './dto/update-factor.dto';
 
-@Controller('factor')
+const ENDPOINT = {
+  VERSION: 'v1',
+  ROOT: 'factors',
+};
+
+@Controller(`${ENDPOINT.VERSION}/${ENDPOINT.ROOT}`)
 export class FactorController {
   constructor(private readonly factorService: FactorService) {}
-
-  @Post()
-  create(@Body() createFactorDto: CreateFactorDto) {
-    return this.factorService.create(createFactorDto);
-  }
 
   @Get()
   findAll() {
     return this.factorService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.factorService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFactorDto: UpdateFactorDto) {
-    return this.factorService.update(+id, updateFactorDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.factorService.remove(+id);
   }
 }
--- src/features/factor/services/factor.service.ts
+++ src/features/factor/services/factor.service.ts
@@ -1,26 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { CreateFactorDto } from '../dto/create-factor.dto';
 import { UpdateFactorDto } from '../dto/update-factor.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Fuel } from '../entities';
+import { DataSource, Repository } from 'typeorm';
 
 @Injectable()
 export class FactorService {
-  create(createFactorDto: CreateFactorDto) {
-    return 'This action adds a new factor';
-  }
+  private readonly _logger = new Logger(FactorService.name);
+  constructor(
+    @InjectRepository(Fuel)
+    private readonly _fuelRepository: Repository<Fuel>,
+    private readonly _dataSource: DataSource,
+  ) {}
 
-  findAll() {
-    return `This action returns all factor`;
-  }
+  async findAll() {
+    let dto: {
+      fuel_id: number;
+      fuel_name: string;
+      unit_name: string;
+      unit_id: number;
+      emission_factor: number;
+    }[] = [];
 
-  findOne(id: number) {
-    return `This action returns a #${id} factor`;
-  }
+    try {
+      dto = await this._dataSource
+        .getRepository(Fuel)
+        .createQueryBuilder('fuel')
+        .innerJoin('fuel.fuelUnits', 'fuelUnits')
+        .innerJoin('fuelUnits.unit', 'unit')
+        .orderBy('fuel.id', 'ASC')
+        .select([
+          'fuel.id',
+          'fuel.name',
+          'unit.name',
+          'unit.id',
+          'fuelUnits.emission_factor',
+        ])
+        .getRawMany();
+    } catch (error) {
+      this._logger.error(error);
+      throw new InternalServerErrorException();
+    }
 
-  update(id: number, updateFactorDto: UpdateFactorDto) {
-    return `This action updates a #${id} factor`;
-  }
+    if (dto.length === 0) {
+      return [];
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} factor`;
+    //mapper
+    return dto.map((item) => ({
+      fuel: {
+        id: item.fuel_id,
+        name: item.fuel_name,
+      },
+      unit: {
+        id: item.unit_id,
+        name: item.unit_name,
+      },
+      emission_factor: item.emission_factor,
+    }));
   }
 }
